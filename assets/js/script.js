f75bd57ffd57bfa94da5c930244783101c5d64ae
Cristian <ckcristock@gmail.com>
2023-08-10 23:46:32 -0500

    2 - good

--- assets/js/script.js
+++ assets/js/script.js
@@ -235,11 +235,11 @@ srtop.reveal(".home .instagram", { interval: 600 });
 srtop.reveal(".home .dev", { interval: 600 });
 
 /* SCROLL ABOUT */
-srtop.reveal(".about .content h3", { delay: 200 });
-srtop.reveal(".about .content .tag", { delay: 200 });
-srtop.reveal(".about .content p", { delay: 200 });
-srtop.reveal(".about .content .box-container", { delay: 200 });
-srtop.reveal(".about .content .resumebtn", { delay: 200 });
+srtop.reveal(".about .content h3", { delay: 110 });
+srtop.reveal(".about .content .tag", { delay: 110 });
+srtop.reveal(".about .content p", { delay: 110 });
+srtop.reveal(".about .content .box-container", { delay: 110 });
+srtop.reveal(".about .content .resumebtn", { delay: 110 });
 
 /* SCROLL SKILLS */
 srtop.reveal(".skills .container", { interval: 200 });
