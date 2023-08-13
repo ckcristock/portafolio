98504460cea4d14e369eaea860d586b8d992ded8
Cristian <ckcristock@gmail.com>
2023-08-13 01:15:06 -0500

    19

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,15 +62,15 @@ $(document).ready(function () {
   // <!-- emailjs to mail contact form data -->
 });
 
-document.addEventListener("visibilitychange", function () {
-  if (document.visibilityState === "visible") {
-    document.title = "Portfolio | Cristian Sable";
-    $("#favicon").attr("href", "assets/images/favicon.png");
-  } else {
-    document.title = "Come Back To Portfolio";
-    $("#favicon").attr("href", "assets/images/favhand.png");
-  }
-});
+// document.addEventListener("visibilitychange", function () {
+//   if (document.visibilityState === "visible") {
+//     document.title = "Portfolio | Cristian Sable";
+//     $("#favicon").attr("href", "assets/images/favicon.png");
+//   } else {
+//     document.title = "Come Back To Portfolio";
+//     $("#favicon").attr("href", "assets/images/favhand.png");
+//   }
+// });
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
